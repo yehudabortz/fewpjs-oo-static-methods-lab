0cae7ce665cd31da209ac22ad51ae81850e375f0
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-31 00:19:36 -0500

    commit tree 33fd3fb1563a86a4cf3d7e62c895561818e059cf

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string ) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(sentence) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let arrayOfWords = sentence.split(" ")
+
+    for (let i = 0; i < arrayOfWords.length; i++)
+      if (i == 0) {
+        result.push(this.capitalize(arrayOfWords[i]))
+      } else {
+        if (exceptions.includes(arrayOfWords[i])) {
+          result.push(arrayOfWords[i])
+        } else {
+          result.push(this.capitalize(arrayOfWords[i]))
+        }
+      }
+    return result.join(" ")
+  }
 }
